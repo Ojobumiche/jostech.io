6ffae97dc49ca88a9e1e09bedecf5a2330eeecd0
Ojobumiche <bumiche@mail.com>
2023-02-23 20:55:45 -0800

    Tech is my passion

---
+++ record.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+
+
+
+int struct
+{
+	char date_of_birth[20];
+	int age;
+	char name[20];
+	char sex;
+	char state;
+} st1, st2, st3;
+
+int main(void)
+{
+	struct st
---
+++ structure.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+struct exam
+{
+	char name[20];
+	int age;
+	char state[20];
+	int year;
+}exam1;
+
+int main(void)
+{
+	exam1.name, "Joshua";
+	exam1.age = 76;
+	exam1.state, "Abia";
+	exam1.year = 1987;
+
+	printf("%s\n", exam1.name);
+
+	printf("%d\n", exam1.year);
+
+	printf("%s\n", exam1.state);
+
+
+
+
+
+
+
+	return (0);
+}
